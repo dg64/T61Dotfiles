7ca5fe922ba639dd116a75665220f02926ab7b93
dg <noone64@gmail.com>
2020-07-08 15:56:04 +0300

    Add .config

---
+++ .config/openbox/autostart.sh
@@ -0,0 +1,7 @@
+nitrogen --restore &
+#tint2 -c ~/.config/tint2/justabar.tint2rc &
+~/.config/polybar/launch.sh &
+compton -CG --config ~/.config/compton/compton.conf &
+#conky -c ~/.config/conky/.conkyrc &
+
+udiskie -s &
